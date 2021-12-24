f070f9bfa9e55ade7f6405767fac6b3ea426fbb8
anandgitacc <anand.akmail@gmail.com>
2021-12-24 11:06:22 +0530

    anandgit

--- src/org/test/Employee.java
+++ src/org/test/Employee.java
@@ -10,6 +10,11 @@ public class Employee {
 		System.out.println("8939278836");
 	}
 	
+	private void empLocation() {
+		
+		System.out.println("OMR, chennai");
+	}
+	
 	public static void main(String[] args) {
 		
 		Employee e = new Employee();
